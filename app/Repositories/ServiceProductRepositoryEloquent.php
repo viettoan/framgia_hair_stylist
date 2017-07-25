0cab9f9edbc08a99b12d06a9d90b5b57849eb5c8
Dinh Tai <tai.dinhvan2702@gmail.com>
2017-07-25 17:19:55 +0700

    Create Update Service

---
+++ app/Repositories/ServiceProductRepositoryEloquent.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Contracts\Repositories\ServiceProductRepository;
+use App\Eloquents\ServiceProduct;
+
+class ServiceProductRepositoryEloquent extends AbstractRepositoryEloquent implements ServiceProductRepository
+{
+    public function model()
+    {
+        return new ServiceProduct;
+    }
+
+    public function create($data)
+    {
+    	return $this->model()->create($data);
+    }
+
+    public function find($id, $with = [], $select = ['*'])
+    {
+    	return $this->model()->select($select)->with($with)->find($id);
+    }
+}
