bba5ac9de8498d9aada63d3bca672cd12317dfbb
Dinh Tai <tai.dinhvan2702@gmail.com>
2017-08-01 15:13:05 +0700

    Edit Bill

--- app/Contracts/Repositories/BillItemRepository.php
+++ app/Contracts/Repositories/BillItemRepository.php
@@ -5,4 +5,8 @@ namespace App\Contracts\Repositories;
 interface BillItemRepository extends AbstractRepository
 {
     public function create($data);
+
+    public function find($id, $with = [], $select = ['*']);
+
+    public function getItemsByBillId($bill_id, $with = [], $select = ['*']);
 }
