6eeb58734eabf7228ffa08e85cb9e848bdc0629d
tranvanmy <anhmynd19101995@gmail.com>
2017-10-06 11:13:53 +0700

    FIX UI BOOKING

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,11 +4,11 @@
 
 @section('content')
     @include('sites._section.head')
-    @include('sites._section.slider')
+    {{-- @include('sites._section.slider') --}}
     @include('sites._section.booking3')
-    @include('sites._section.hair_style')
-    @include('sites._section.about')
-    @include('sites._section.service')
-    @include('sites._section.testimonials')
-    @include('sites._section.footer')
+    {{-- @include('sites._section.hair_style') --}}
+    {{-- @include('sites._section.about') --}}
+    {{-- @include('sites._section.service') --}}
+    {{-- @include('sites._section.testimonials') --}}
+    {{-- @include('sites._section.footer') --}}
 @endsection
